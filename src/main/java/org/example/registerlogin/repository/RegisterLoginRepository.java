3795d42cf42e8f9fb93123682d4ef65a1758415e
JONATHANCRY <thieuprono1@gmail.com>
2025-11-18 17:05:15 +0700

    xoá comment

--- src/main/java/org/example/registerlogin/repository/RegisterLoginRepository.java
+++ src/main/java/org/example/registerlogin/repository/RegisterLoginRepository.java
@@ -6,14 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegisterLoginRepository extends JpaRepository<UserEntity,Long> {
-    // tìm email = email
-    // SELECT * FROM register WHERE email = ?; trả về 1 RegisterEntity
+
     UserEntity findByEmail(String email);
-
-    // kiểm tra giá trị email đã tồn tại chưa, trả về giá trị true, false
     boolean existsByEmail(String email);
-
-    // tìm verification_token = token
-    // SELECT * FROM register WHERE verification_token = ?;
     UserEntity findByVerificationToken(String token);
 }
