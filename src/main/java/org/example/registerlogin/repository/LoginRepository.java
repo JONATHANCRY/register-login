e7886aeb6ce0cfaf7102da955cfb7616d65bca2d
JONATHANCRY <thieuprono1@gmail.com>
2025-11-19 09:51:13 +0700

    xoá comment

--- src/main/java/org/example/registerlogin/repository/LoginRepository.java
+++ src/main/java/org/example/registerlogin/repository/LoginRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoginRepository extends JpaRepository<UserEntity,Long> {
-    // tìm email = email
-    // SELECT * FROM register WHERE email = ?; trả về 1 RegisterEntity
+
     UserEntity findByEmail(String email);
 }
--- src/main/java/org/example/registerlogin/repository/RegisterRepository.java
+++ src/main/java/org/example/registerlogin/repository/RegisterRepository.java
@@ -6,24 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegisterRepository extends JpaRepository<UserEntity,Long> {
-    // kiểm tra giá trị email đã tồn tại chưa, trả về giá trị true, false
-    boolean existsByEmail(String email);
 
-    // tìm verification_token = token
-    // SELECT * FROM register WHERE verification_token = ?;
+    boolean existsByEmail(String email);
     UserEntity findByVerificationToken(String token);
 
 }
-
-//RegisterEntity,Long là tên class(entity) và khoá chính của bảng
-
-//   Tự động có sẵn các method CRUD như:
-//    // save(), findAll(), findById(), deleteById(), existsById()
-
-// tự tạo các hàm mới chỉ bằng cách đặt tên theo quy tắc Spring JPA,
-// Spring sẽ tự động tạo câu SQL tương ứng.
-
-//Repository trong Spring Data JPA là lớp trung gian giữa ứng dụng và database.
-//Nó giúp bạn:
-//Không cần tự viết SQL thủ công
-//Gọi các hàm có sẵn như save(), findAll(), findById(), deleteById(), v.v.
